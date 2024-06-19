d0879641b8887346265dc624287b207a78bb488f
bmgeier <geier.benjamin@gmail.com>
2024-06-19 17:09:05 -0400

    final commit before presentation

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -29,26 +29,33 @@ function handleSubmit(event){
 }
 
 return(
-    <form className='user_form' onSubmit={handleSubmit}>
+    <form className='bg-slate-300 p-6 rounded-lg shadow-lg w-full max-w-md mx-auto' onSubmit={handleSubmit}>
         <h3>Log In!</h3>
-
+        <div className='mb-4'>
+        <label className='block font-medium mb-2'>Username
         <input type="text"
         name='username'
         onChange={e => setUserName(e.target.value)}
         placeholder = 'Username'
         value={username}
+        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
         />
-
+        </label>
+        </div>
+        <div className='mb-4'>
+        <label className='block font-medium mb-2'>Password
         <input type="password"
         name='password'
         onChange={e => setPassword(e.target.value)}
         placeholder = 'Password'
         value={password}
+        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
         />
-
+        </label>
         <input type="submit"
         value='Log In'
         />
+        </div>
 
 
     </form>
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -31,48 +31,66 @@ function handleSubmit(event){
 }
 
 return(
-    <form className='user_form' onSubmit={handleSubmit}>
-        <h3>Signup</h3>
-
-        <input type="text"
-        name='username'
-        onChange={e => setUserName(e.target.value)}
-        placeholder = 'Username'
-        value={username}
-        />
-        <input type="number"
-        name='age'
-        onChange={e => setAge(e.target.value)}
-        placeholder = 'Age'
-        value={age}
-        />
-
-        <input type="password"
-        name='password'
-        onChange={e => setPassword(e.target.value)}
-        placeholder = 'Password'
-        value={password}
-        />
-
-        <input type="text"
-        name= 'email_address'
-        onChange={e => setEmail(e.target.value)}
-        placeholder = 'Email address'
-        value={email_address}
-        />
-
-        <textarea
-        name='bio'
-        onChange={e => setBio(e.target.value)}
-        placeholder = 'Bio'
-        value={bio}
-        />
-
+    <form className='bg-slate-300 p-6 rounded-lg shadow-lg w-full max-w-md mx-auto' onSubmit={handleSubmit}>
+        <h3 className='text-2xl font-semibold mb-6 text-gray-700'>Signup</h3>
+        <div className='mb-4'>
+            <label className='block font-medium mb-2'>Username:
+                <input type="text"
+                name='username'
+                onChange={e => setUserName(e.target.value)}
+                placeholder = 'Username'
+                value={username}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+        </label>
+        </div>
+        <div className='mb-4'>
+            <lable className='block font-medium mb-2'>Age:
+                <input type="number"
+                name='age'
+                onChange={e => setAge(e.target.value)}
+                placeholder = 'Age'
+                value={age}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+            </lable>
+        </div> 
+        <div className='mb-4'>
+            <label className='block font-medium mb-2'>Password
+                <input type="password"
+                name='password'
+                onChange={e => setPassword(e.target.value)}
+                placeholder = 'Password'
+                value={password}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+            </label>
+        </div>
+        <div className='mb-4'>   
+            <label className='block font-medium mb-2'>Email address:
+                <input type="text"
+                name= 'email_address'
+                onChange={e => setEmail(e.target.value)}
+                placeholder = 'Email address'
+                value={email_address}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+        </label>
+        </div>
+        <div className='mb-4'>  
+            <label className='block font-medium mb-2'>Bio:
+                <textarea
+                name='bio'
+                onChange={e => setBio(e.target.value)}
+                placeholder = 'Bio'
+                value={bio}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            />
+            </label>
+        </div>
         <input type="submit"
         value='Signup'
         />
-
-
     </form>
 )
 
